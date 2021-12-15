473d897f0f2a4a3b659f5da0e732949bf0ca3fb2
Momen Abdelwadoud <momenwadoudg@gmail.com>
2021-12-15 10:29:18 +0200

    First official commit. Ready to code

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          blue: "hsl(215, 51%, 70%)",
+          cyan: "hsl(178, 100%, 50%)",
+        },
+      },
+      fontFamily: {
+        main: ["Outfit"],
+      },
+    },
+  },
+  plugins: [],
+};
